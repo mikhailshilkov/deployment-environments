dfa0552596992b11268f10325651289c291effc6
Mikhail Shilkov <github@mikhail.io>
2024-04-15 14:43:24 +0200

    Add C# and TS

---
+++ Environments/PulumiCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.Storage;
+using Pulumi.AzureNative.Storage.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var config = new Config();
+    var resourceGroupName = config.Require("resource-group-name");
+    var skuName = config.Get("sku") ?? "Standard_LRS";
+    var storageAccount = new StorageAccount("sa", new StorageAccountArgs
+    {
+        ResourceGroupName = resourceGroupName,
+        Sku = new SkuArgs
+        {
+            Name = skuName
+        },
+        Kind = Kind.StorageV2
+    });
+    return new Dictionary<string, object?>();
+});

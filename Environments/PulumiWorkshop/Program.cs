ad526d2d6f0b93af4795cae1a00e2f5fee1e5716
Mikhail Shilkov <github@mikhail.io>
2024-06-17 11:50:57 +0200

    Workshop env

---
+++ Environments/PulumiWorkshop/Program.cs
@@ -0,0 +1,53 @@
+﻿using Pulumi;
+using Pulumi.AzureNative.OperationalInsights;
+using Pulumi.AzureNative.OperationalInsights.Inputs;
+using Pulumi.AzureNative.Resources;
+using Pulumi.AzureNative.App;
+using Pulumi.AzureNative.App.Inputs;
+using System.Collections.Generic;
+
+return await Pulumi.Deployment.RunAsync(() =>
+{
+    var config = new Config();
+    
+    // Get the resource group name provisioned by ADE.
+    var resourceGroupName = config.Require("resource-group-name");
+
+    // Read the container image name parameter.
+    var imageName = config.Require("image");
+
+    var managedEnv = new ManagedEnvironment("env", new ManagedEnvironmentArgs
+    {
+        ResourceGroupName = resourceGroupName
+    });
+    
+    var containerApp = new ContainerApp("app", new()
+    {
+        ResourceGroupName = resourceGroupName,
+        ManagedEnvironmentId = managedEnv.Id,
+        Configuration = new ConfigurationArgs
+        {
+            Ingress = new IngressArgs
+            {
+                External = true,
+                TargetPort = 80
+            },
+        },
+        Template = new TemplateArgs
+        {
+            Containers = 
+            {
+                new ContainerArgs
+                {
+                    Name = "myapp",
+                    Image = imageName,
+                }
+            }
+        }
+    });
+
+    // Return outputs.
+    return new Dictionary<string, object?>{
+        ["url"] = Output.Format($"https://{containerApp.Configuration.Apply(c => c!.Ingress).Apply(i => i!.Fqdn)}")
+    };
+});
